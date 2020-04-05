2e31e677d1171456187f2f302345dfc043a40353
Jay Nandy <jayjaynandy@gmail.com>
2020-04-05 15:27:49 +0800

    Update Local.py

--- mnist/Local.py
+++ mnist/Local.py
@@ -6,7 +6,7 @@ These values will be updated as we execute the code
 nodeCount = 0
 updateMode = 0
 batchCount = 15
-desiredNodeCount = 64.0
+desiredNodeCount = 10.0
 alpha = 0.0
 
 memberThreshold = 0.0
