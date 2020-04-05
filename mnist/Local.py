384add77f29de7f6aee55b2aa19583f93b999523
Jay Nandy <jayjaynandy@gmail.com>
2020-04-05 15:07:13 +0800

    Add files via upload

---
+++ mnist/Local.py
@@ -0,0 +1,13 @@
+''' 
+No need to change these values. 
+These values will be updated as we execute the code
+'''
+
+nodeCount = 0
+updateMode = 0
+batchCount = 15
+desiredNodeCount = 64.0
+alpha = 0.0
+
+memberThreshold = 0.0
+countThreshold = 1
